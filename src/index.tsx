81d961875c763a587a28a9332f1c32877a7b1481
Andrey Soroko <supersoroko@gmail.com>
2023-06-15 14:24:46 +0300

    Ref: project architecture, configure global imports;

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom';
-import App from "./App";
-import "./styles/index.scss";
+import {render} from "react-dom";
+import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
-import ThemeProvider from "./theme/ThemeProvider";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 
-ReactDOM.render(
+
+render(
     <BrowserRouter>
         <ThemeProvider>
-            <App/>
+            <App />
         </ThemeProvider>
     </BrowserRouter>,
     document.getElementById('root')
-);
+)
