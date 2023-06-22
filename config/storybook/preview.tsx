6adf230b9ade9844164040e6c9aa87563bf20d17
Andrey Soroko <supersoroko@gmail.com>
2023-06-22 14:49:32 +0300

    feat: main pipeline github actions

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,10 +1,9 @@
-import type { Preview } from "@storybook/react"
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator"
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator"
 import { Theme } from "../../src/app/providers/ThemeProvider"
 import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator"
 
-const preview: Preview = {
+const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
