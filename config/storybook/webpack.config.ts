a8a1590d66d142036a6cb398d797f08a45aa2c13
Andrey Soroko <supersoroko@gmail.com>
2023-06-22 23:01:26 +0300

    fix: packages versions

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,23 +1,24 @@
 import webpack, { RuleSetRule } from "webpack"
-import { BuildPaths } from "../build/types/config"
 import path from "path"
 import { buildCssLoader } from "../build/loaders/buildCssLoader"
+import { BuildPaths } from "../build/types/config"
 
 export default ({ config }: {config: webpack.Configuration}) => {
   const paths: BuildPaths = {
     build: "",
-    entry: "",
     html: "",
+    entry: "",
     src: path.resolve(__dirname, "..", "..", "src"),
   }
   config.resolve.modules.push(paths.src)
-
   config.resolve.extensions.push(".ts", ".tsx")
 
-  config.module.rules = config.module.rules.map( (rule: RuleSetRule) => {
+  // eslint-disable-next-line no-param-reassign
+  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i }
     }
+
     return rule
   })
 
@@ -26,5 +27,6 @@ export default ({ config }: {config: webpack.Configuration}) => {
     use: ["@svgr/webpack"],
   })
   config.module.rules.push(buildCssLoader(true))
+
   return config
 }
